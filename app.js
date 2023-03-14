9ebd00e020095ab83ed965bba1995d9e068bd286
kharboutmanel98 <kharbout2017@gmail.com>
2023-03-14 21:05:28 +0100

    First commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const authRouter = require('./routes/auth');
+const movieRouter = require('./routes/movies');
+
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/movies', movieRouter)
+
+
+mongoose.set('strictQuery' , true) // to suppress DeprecationWarning to connect to mongo db using mongoose
+mongoose.connect(process.env.DB_URL)
+module.exports = app;
---
+++ middlewares/admin.js
@@ -0,0 +1,9 @@
+const User = require('../models/user');
+
+exports.check = async(req, res, next)=>{
+    const user = await User.findById(req.userId)
+
+    user.IsAdmin ? next() : res.status(403).json({
+        message: 'forbidden'
+    })
+}
